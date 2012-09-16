43d9bf681531e85afb4543ff91ccb75b48d6dd74
tokuhirom <tokuhirom@gmail.com>
2012-09-16 15:24:54 +0900

    initial import

---
+++ lib/Net/DNS/Paranoid.pm
@@ -0,0 +1,39 @@
+package Net::DNS::Paranoid;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Net::DNS::Paranoid - A module for you
+
+=head1 SYNOPSIS
+
+  use Net::DNS::Paranoid;
+
+=head1 DESCRIPTION
+
+Net::DNS::Paranoid is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
