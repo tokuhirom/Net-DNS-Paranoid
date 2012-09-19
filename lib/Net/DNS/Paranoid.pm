0cfec9aa52d00e6db8226392aee4b057c335cd29
tokuhirom <tokuhirom@gmail.com>
2012-09-19 10:42:34 +0900

    Checking in changes prior to tagging of version 0.07.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 56f2a5c..b40946c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension Net::DNS::Paranoid
    
    +0.07
    +
    +    - fixed deps
    +
     0.06
    
         - more better dist packaging

--- lib/Net/DNS/Paranoid.pm
+++ lib/Net/DNS/Paranoid.pm
@@ -2,7 +2,7 @@ package Net::DNS::Paranoid;
 use strict;
 use warnings;
 use 5.008008;
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use Class::Accessor::Lite (
     rw => [qw(timeout blocked_hosts whitelisted_hosts resolver)]
